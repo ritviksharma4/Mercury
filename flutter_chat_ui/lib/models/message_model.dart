83776fd6b83d72de339ff48764feff97cf1d981e
Ritvik Sharma <ritviksharma4@github.com>
2020-07-29 12:12:33 +0530

    ChatBot UI ; KeyBoard and Speech Icon TBF

---
+++ flutter_chat_ui/lib/models/message_model.dart
@@ -0,0 +1,82 @@
+import 'package:flutter_chat_ui/models/user_model.dart';
+
+class Message {
+  final User sender;
+  final String
+      time; // Would usually be type DateTime or Firebase Timestamp in production apps
+  final String text;
+
+
+  Message({
+    this.sender,
+    this.time,
+    this.text,
+  });
+}
+
+// YOU - current user
+final User Ritvik = User(
+  id: 0,
+  name: 'Ritvik',
+  imageUrl: 'assets/images/user.jpg',
+);
+
+// USERS
+final User ChatBot = User(
+  id: 1,
+  name: 'ChatBot',
+  imageUrl: 'assets/images/chatbot.jpg',
+);
+
+
+// FAVORITE CONTACTS
+List<User> favorites = [ChatBot];
+
+List<Message> messages = [
+
+  Message(
+    sender: ChatBot,
+    time: '4:30 PM',
+    text: 'Nice to hear, See ya!',
+  ),
+  Message(
+    sender: Ritvik,
+    time: '4:30 PM',
+    text: 'All good here too :)',
+  ),
+  Message(
+    sender: ChatBot,
+    time: '4:30 PM',
+    text: 'Hey, its all good today and you?',
+  ),
+  Message(
+    sender: Ritvik,
+    time: '4:30 PM',
+    text: 'Hey, how\'s it going? What did you do today?1',
+  ),
+  Message(
+    sender: Ritvik,
+    time: '4:30 PM',
+    text: 'Hey, how\'s it going? What did you do today?2',
+  ),
+  Message(
+    sender: ChatBot,
+    time: '4:30 PM',
+    text: 'Hey, let\'s talk!',
+  ),
+];
+
+// EXAMPLE CHATS ON HOME SCREEN
+List<Message> chats = [
+//  Message(
+//    sender: Ritvik,
+//    time: '4:30 PM',
+//    text: 'Hey, how\'s it going? What did you do today?',
+//  ),
+  Message(
+    sender: ChatBot,
+    time: '4:30 PM',
+    text: 'Nice to hear, See ya!',
+  )
+];
+
